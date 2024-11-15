a523d694157a633240e86c3ce7c1a7e7a4a1ca26
HasinduNavanjana <navanjanahasindu@gmail.com>
2024-11-15 20:00:51 +0530

    remove shopping cart button from navbar on mobile view

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -110,9 +110,6 @@ const Navbar = () => {
                   <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                   <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                 </Button>
-                <Button variant="ghost" size="icon" className="hover:bg-muted">
-                  <ShoppingCart className="h-5 w-5" />
-                </Button>
               </div>
             </div>
           </div>
